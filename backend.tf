81be5800639d8bb4a41642dcbb0cad532310ba1d
Xia <xiaweigu@amazon.com>
2025-07-15 16:39:54 +0800

    commit

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "test-eks-cluster-terraform-state"
-    key            = "dify-enterprise/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "test-eks-cluster-terraform-locks"
-    encrypt        = true
-  }
-}
